38aae38377bc0df61cce1c822f2095651d6f02a7
Joel <joebukkit@gmail.com>
2014-11-15 06:55:06 -0600

    Stuff and things.

--- cgi-bin/head.php
+++ cgi-bin/head.php
@@ -2,9 +2,10 @@
   <meta charset="UTF-8" />
   <title><?php
     if(isset($title)) {
-      echo $title.' | ';
+      echo $title . ' | ';
     }
   ?>MineHoes</title>
   <link rel="stylesheet" href="/reset.css" />
+  <link rel="stylesheet" href="/fonts.css" />
   <link rel="stylesheet" href="/main.css" />
 </head>
--- index.php
+++ index.php
@@ -4,13 +4,9 @@
 <html lang="en">
   <?php require 'cgi-bin/head.php'; ?>
   <body>
+    <?php require 'cgi-bin/navigation.php'; ?>
     <h1>Hello And Welcome to MineHoes the best Of the Fucking west!
     We sell jelly beans and pinto beans FULL OF FUCKING FLAVOR 
     </h1>
-    <nav>
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
   </body>
 </html>
