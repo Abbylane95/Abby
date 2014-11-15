6c7f7e307f2d94e952ab2e6d0c79a452e02d9024
Joel <joebukkit@gmail.com>
2014-11-15 06:27:30 -0600

    Add head.php, more reset.

---
+++ cgi-bin/head.php
@@ -0,0 +1,10 @@
+<head>
+  <meta charset="UTF-8" />
+  <title><?php
+    if(isset($title)) {
+      echo $title.' | ';
+    }
+  ?>MineHoes</title>
+  <link rel="stylesheet" href="/reset.css" />
+  <link rel="stylesheet" href="/main.css" />
+</head>
--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
+<?php
+  $title = 'Home';
+?><!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>MineHoes</title>
-    <link rel="stylesheet" href="/reset.css" />
-    <link rel="stylesheet" href="/main.css" />
-    </head>
-    <body>
+  <?php require 'cgi-bin/head.php'; ?>
+  <body>
     <h1>Hello And Welcome to MineHoes the best Of the Fucking west!
     We sell jelly beans and pinto beans FULL OF FUCKING FLAVOR 
     </h1>
