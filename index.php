c82a819859433b5505029fac619565833c2d7535
Joel <joebukkit@gmail.com>
2014-11-15 05:58:39 -0600

    reset.css

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
   <head>
     <meta charset="utf-8" />
     <title>Example</title>
+    <link rel="stylesheet" href="/reset.css" />
     <link rel="stylesheet" href="/main.css" />
   </head>
   <body>
-    
+    <nav>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
   </body>
 </html>
